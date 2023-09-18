ada2db51a6378bb93128b3c17477b78eb2dd4ae1
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-09-18 18:37:28 +0100

    initial commit

---
+++ src/test/java/com/calsam/MealPlanner/MealPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calsam.MealPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
