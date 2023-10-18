74bfc73d6210f38ac844e2be73b43ef35e10396d
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-10-18 16:29:54 +0100

    created JWTAuthorizationFilter

---
+++ src/main/java/com/calsam/MealPlanner/filters/JWTAuthorizationFilter.java
@@ -0,0 +1,61 @@
+package com.calsam.MealPlanner.filters;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static com.calsam.MealPlanner.configurations.SecurityConstants.*;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+    public JWTAuthorizationFilter(AuthenticationManager authManager) {
+        super(authManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+        throws IOException, ServletException {
+        String header = request.getHeader(HEADER_STRING);
+
+        if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+            chain.doFilter(request, response);
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
+    // Reads the JWT from the Authorization header, and then uses JWT to validate the token
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String token = request.getHeader(HEADER_STRING);
+
+        if (token != null) {
+            // parse the token.
+            String user = JWT.require(Algorithm.HMAC512(SECRET.getBytes()))
+                    .build()
+                    .verify(token.replace(TOKEN_PREFIX, ""))
+                    .getSubject();
+
+            if (user != null) {
+                // new arraylist means authorities
+                return new UsernamePasswordAuthenticationToken(user, null, new ArrayList<>());
+            }
+
+            return null;
+        }
+
+        return null;
+    }
+
+}
