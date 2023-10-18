a9b8714dfbf7869d6cbdfe2db22f4343b413ab7d
Callum BP <122090016+cbattenplowright@users.noreply.github.com>
2023-10-18 16:58:45 +0100

    cannot understand the code around the security config file

--- src/main/java/com/calsam/MealPlanner/configurations/SecurityConfiguration.java
+++ src/main/java/com/calsam/MealPlanner/configurations/SecurityConfiguration.java
@@ -1,22 +1,59 @@
 package com.calsam.MealPlanner.configurations;
 
+import com.calsam.MealPlanner.filters.JWTAuthorizationFilter;
+import org.springframework.security.authentication.AuthenticationManager;
+import com.calsam.MealPlanner.filters.JWTAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import static com.calsam.MealPlanner.configurations.SecurityConstants.SIGN_UP_URL;
 
 @Configuration
 public class SecurityConfiguration {
+
+    private UserDetailsService userDetailsService;
+
+    private AuthenticationManager authenticationManager;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((authz) -> authz.anyRequest().permitAll()).httpBasic(withDefaults());
+        http.cors().and().authorizeHttpRequests().antMatchers(HttpMethod.POST, SIGN_UP_URL)
+                .permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
         http.csrf(AbstractHttpConfigurer::disable);
         //http.csrf().disable(); --> this version is deprecated
 
         return http.build();
     }
+
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
+
 }
