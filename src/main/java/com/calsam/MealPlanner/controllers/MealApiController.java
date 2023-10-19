d6d35f46c9f95918cffc8be55e85a58f01311da6
sam98963 <szwilliamson01@gmail.com>
2023-10-19 14:52:58 +0100

    config file needs editing

--- src/main/java/com/calsam/MealPlanner/controllers/MealApiController.java
+++ src/main/java/com/calsam/MealPlanner/controllers/MealApiController.java
@@ -1,12 +1,10 @@
 package com.calsam.MealPlanner.controllers;
 import com.calsam.MealPlanner.models.Meal;
-import com.calsam.MealPlanner.models.MealResponse;
 import com.calsam.MealPlanner.services.MealApiService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
